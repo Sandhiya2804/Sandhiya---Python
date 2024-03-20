feb160209360256fac2da7e0906cf9a3f782d2cb
Sandhiya2804 <146424947+Sandhiya2804@users.noreply.github.com>
2024-03-19 23:11:29 -0700

    Add files via upload

---
+++ calender usingpython.py
@@ -0,0 +1,10 @@
+# Program to display calendar of the given month and year
+
+# importing calendar module
+import calendar
+
+yy = int(input("enter the year"))
+mm = int(input("enter the month"))
+
+#display the output
+print(calender.month(yy, mm))
